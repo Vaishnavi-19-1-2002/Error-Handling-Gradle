9143914c2dc6c4e18d53792813b3c93c3804fffe
Vaishnavi Santosh Bhajibhakare <73975278+Vaishnavi-19-1-2002@users.noreply.github.com>
2023-12-18 20:29:00 +0530

    Add files via upload

---
+++ src/main/java/ErrorHandling.java
@@ -0,0 +1,41 @@
+import java.util.Optional;
+class ErrorHandling {
+    void handleErrorByThrowingIllegalArgumentException() {
+        throw new IllegalArgumentException();
+    }
+    void handleErrorByThrowingIllegalArgumentExceptionWithDetailMessage(String message) {
+        throw new IllegalArgumentException(message);
+    }
+    void handleErrorByThrowingAnyCheckedException() throws CustomCheckedException{
+        throw new CustomCheckedException("This is the detail message.");
+    }
+    void handleErrorByThrowingAnyCheckedExceptionWithDetailMessage(String message) throws CustomCheckedException{
+        throw new CustomCheckedException(message);
+    }
+    void handleErrorByThrowingAnyUncheckedException() {
+        throw new CustomUncheckedException();
+    }
+    void handleErrorByThrowingAnyUncheckedExceptionWithDetailMessage(String message) {
+        throw new CustomUncheckedException(message);
+    }
+    void handleErrorByThrowingCustomCheckedException() throws CustomCheckedException{
+        throw new CustomCheckedException();
+    }
+    void handleErrorByThrowingCustomCheckedExceptionWithDetailMessage(String message) throws CustomCheckedException {
+        throw new CustomCheckedException(message);
+    }
+    void handleErrorByThrowingCustomUncheckedException() {
+        throw new CustomUncheckedException();
+    }
+    void handleErrorByThrowingCustomUncheckedExceptionWithDetailMessage(String message) {
+        throw new CustomUncheckedException(message);
+    }
+    Optional<Integer> handleErrorByReturningOptionalInstance(String integer) {
+        try {
+            return Optional.of(Integer.parseInt(integer));
+        } catch (Exception e) {
+            System.out.print("Exception thrown: " + e);
+        }
+        return Optional.empty();
+    }
+}
